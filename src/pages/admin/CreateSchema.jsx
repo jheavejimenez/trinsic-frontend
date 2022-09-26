44b9facc40fb3d4ddb797f7f1486d8930dcf80ce
jheave <jimenezjheave123@gmail.com>
2022-09-26 18:50:44 +0800

    add add schema btn

--- src/pages/admin/CreateSchema.jsx
+++ src/pages/admin/CreateSchema.jsx
@@ -1,34 +1,42 @@
 import React from "react";
-import { Container, Heading, SimpleGrid, Stack, Text, useColorModeValue } from "@chakra-ui/react";
+import { Box, Container, Flex, Heading, SimpleGrid, Spacer, Stack, Text, useColorModeValue } from "@chakra-ui/react";
+import CustomButton from "../../components/Button";
 
 function CreateSchema() {
     return (
-        <Container maxW={'1280px'} marginTop={'10vh'}>
-            <SimpleGrid columns={3} spacing={5}>
-                <Stack
-                    boxShadow={'2xl'}
-                    bg={useColorModeValue('white', 'gray.700')}
-                    rounded={'xl'}
-                    p={10}
-                    spacing={8}
-                    align={'center'}>
-                    <Stack align={'right'} spacing={2}>
-                        <Heading
-                            fontSize={'3xl'}
-                            color={useColorModeValue('gray.800', 'gray.200')}>
-                            {"Competent person"}
-                        </Heading>
-                        <Text fontSize={'lg'} color={'gray.500'}>
-                            {"Competent person"}
-                                " Vituperatoribus utamur facilisi dicat cetero verear luptatum quaeque tota. " +
-                                "Tristique ultricies ac saepe facilisi expetendis platea sagittis erroribus quidam." +
-                                " Reque harum volumus volumus neglegentur movet penatibus."
-                            }
-                        </Text>
+        <>
+            <Container maxW={'1280px'} marginTop={'10vh'}>
+                <Flex>
+                    <Spacer />
+                    <Box>
+                        <CustomButton title={"Add Schema"} path={"/add-schema"} />
+                    </Box>
+                </Flex>
+                <SimpleGrid columns={3} spacing={5}>
+                    <Stack
+                        boxShadow={'2xl'}
+                        bg={useColorModeValue('white', 'gray.700')}
+                        rounded={'xl'}
+                        p={10}
+                        spacing={8}
+                        align={'center'}>
+                        <Stack align={'right'} spacing={2}>
+                            <Heading
+                                fontSize={'3xl'}
+                                color={useColorModeValue('gray.800', 'gray.200')}>
+                                {"Competent person"}
+                            </Heading>
+                            <Text fontSize={'lg'} color={'gray.500'}>
+                                {" Vituperatoribus utamur facilisi dicat cetero verear luptatum quaeque tota. " +
+                                    "Tristique ultricies ac saepe facilisi expetendis platea sagittis erroribus quidam." +
+                                    " Reque harum volumus volumus neglegentur movet penatibus."
+                                }
+                            </Text>
+                        </Stack>
                     </Stack>
-                </Stack>
-            </SimpleGrid>
-        </Container>
+                </SimpleGrid>
+            </Container>
+        </>
     )
 }
 
