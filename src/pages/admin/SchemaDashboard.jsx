1d7dd3661b9fb66cf1f9269bf7d03481b79d3eed
jheave <jimenezjheave123@gmail.com>
2022-09-27 15:05:01 +0800

    add loading animation

--- src/pages/admin/SchemaDashboard.jsx
+++ src/pages/admin/SchemaDashboard.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from "react";
-import { Box, Container, Flex, Heading, SimpleGrid, Spacer, Stack, Text } from "@chakra-ui/react";
+import { Box, Center, Container, Flex, Heading, SimpleGrid, Spacer, Spinner, Stack, Text } from "@chakra-ui/react";
 import CustomButton from "../../components/Button";
 import { credentialsClient } from "../../repository/apiConfig";
 
 function SchemaDashboard() {
     const [schema, setSchema] = React.useState([]);
+    const [loading, setLoading] = React.useState(true);
 
     async function getSchema() {
         let credentialDefinitions = await credentialsClient.listCredentialDefinitions();
-
+        setLoading(false);
         setSchema(credentialDefinitions);
         return credentialDefinitions;
     }
@@ -20,37 +21,51 @@ function SchemaDashboard() {
     return (
         <>
             <Container maxW={'1280px'} marginTop={'10vh'}>
-                <Flex>
-                    <Spacer />
-                    <Box>
-                        <CustomButton title={"Add Schema"} path={"/add-schema"} />
-                    </Box>
-                </Flex>
-                <SimpleGrid columns={3} spacing={5}>
-                    {schema.map((schema, index) => (
-                        <Stack
-                            key={index}
-                            boxShadow={'2xl'}
-                            rounded={'xl'}
-                            p={10}
-                            spacing={8}
-                            align={'center'}>
-                            <Stack align={'right'} spacing={2}>
-                                <Heading
-                                    fontSize={'3xl'}
-                                >
-                                    {schema.name}
-                                </Heading>
-                                <Text fontSize={'md'} color={'gray.500'}>
-                                    {`Revocable: ${schema.supportsRevocation}`}
-                                </Text>
-                                <Text fontSize={'md'} color={'gray.500'}>
-                                    {`Schema ID: ${schema.schemaId}`}
-                                </Text>
-                            </Stack>
-                        </Stack>
-                    ))}
-                </SimpleGrid>
+                {loading ? (
+                    <Center>
+                        <Spinner
+                            thickness='4px'
+                            speed='0.65s'
+                            emptyColor='gray.200'
+                            color='blue.500'
+                            size='xl'
+                        />
+                    </Center>
+                ) : (
+                    <>
+                        <Flex>
+                            <Spacer />
+                            <Box>
+                                <CustomButton title={"Add Schema"} path={"/add-schema"} />
+                            </Box>
+                        </Flex>
+                        <SimpleGrid columns={3} spacing={5}>
+                            {schema.map((schema, index) => (
+                                <Stack
+                                    key={index}
+                                    boxShadow={'2xl'}
+                                    rounded={'xl'}
+                                    p={10}
+                                    spacing={8}
+                                    align={'center'}>
+                                    <Stack align={'right'} spacing={2}>
+                                        <Heading
+                                            fontSize={'3xl'}
+                                        >
+                                            {schema.name}
+                                        </Heading>
+                                        <Text fontSize={'md'} color={'gray.500'}>
+                                            {`Revocable: ${schema.supportsRevocation}`}
+                                        </Text>
+                                        <Text fontSize={'md'} color={'gray.500'}>
+                                            {`Schema ID: ${schema.schemaId}`}
+                                        </Text>
+                                    </Stack>
+                                </Stack>
+                            ))}
+                        </SimpleGrid>
+                    </>
+                )}
             </Container>
         </>
     )
