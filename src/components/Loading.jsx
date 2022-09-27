c8a89730b95fb33afdf716a1f5eb677c9dc99488
jheave <jimenezjheave123@gmail.com>
2022-09-27 17:31:33 +0800

    add loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Center, Spinner } from "@chakra-ui/react";
+
+function Loading() {
+    return (
+        <Center>
+            <Spinner
+                thickness='4px'
+                speed='0.65s'
+                emptyColor='gray.200'
+                color='blue.500'
+                size='xl'
+            />
+        </Center>
+    );
+}
+
+export default Loading;
