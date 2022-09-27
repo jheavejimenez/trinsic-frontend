07f930c753c5cdc9af2d444a14d41182f9f735b9
jheave <jimenezjheave123@gmail.com>
2022-09-27 18:12:23 +0800

    fix bug

--- src/pages/admin/AddSchema.jsx
+++ src/pages/admin/AddSchema.jsx
@@ -1,7 +1,8 @@
-import { Box, Button, Flex, FormControl, FormLabel, Input, Stack, useColorModeValue } from "@chakra-ui/react";
+import { Box, Button, Flex, FormControl, FormLabel, Input, Stack } from "@chakra-ui/react";
 import { useState } from "react";
 import { credentialsClient } from "../../repository/apiConfig";
 import { useNavigate } from "react-router-dom";
+import Loading from "../../components/Loading";
 
 function AddSchema() {
     const [schemaName, setSchemaName] = useState('');
@@ -13,69 +14,82 @@ function AddSchema() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        let credentialDefinition = await credentialsClient.createCredentialDefinition({
-            name: schemaName,
-            version: schemaVersion,
-            attributes: [schemaAttributes],
-            supportRevocation: true, // Enable revocation at a later date
-            tag: "Default" // Tag to identify the schema
-        });
-        console.log(credentialDefinition);
-        // navigate('/admin/schema');
+        try {
+            setLoading(true);
+            let credentialDefinition = await credentialsClient.createCredentialDefinition({
+                name: schemaName,
+                version: schemaVersion,
+                attributes: schemaAttributes.split(',').map((attribute) => attribute.trim()), // split by comma and remove spaces
+                supportRevocation: true, // Enable revocation at a later date
+                tag: "Default" // Tag to identify the schema
+            });
+            console.log(credentialDefinition);
+            setLoading(false);
+            navigate('/admin/schema');
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
     }
 
     return (
-        <Flex
-            minH={'100vh'}
-            align={'center'}
-            justify={'center'}
-            bg={useColorModeValue('gray.50', 'gray.800')}>
-            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-                <Box
-                    rounded={'lg'}
-                    boxShadow={'lg'}
-                    p={8}>
-                    <form onSubmit={handleSubmit}>
-                        <Stack spacing={4}>
-                            <FormControl>
-                                <FormLabel>Schema Name</FormLabel>
-                                <Input
-                                    autoComplete={"off"}
-                                    placeholder='Schema Name'
-                                    value={schemaName}
-                                    onChange={(e) => setSchemaName(e.target.value)}
-                                />
-                                <FormLabel>Schema Version</FormLabel>
-                                <Input
-                                    autoComplete={"off"}
-                                    placeholder='Schema Version'
-                                    value={schemaVersion}
-                                    onChange={(e) => setSchemaVersion(e.target.value)}
-                                />
-                                <FormLabel>Schema Attributes</FormLabel>
-                                <Input
-                                    autoComplete={"off"}
-                                    placeholder='Attribute A, Attribute B'
-                                    value={schemaAttributes}
-                                    onChange={(e) => setSchemaAttributes(e.target.value)}
-                                />
-                            </FormControl>
-                            <Stack spacing={10}>
-                                <Button
-                                    type={'submit'}
-                                    bg={'blue.400'}
-                                    color={'white'}
-                                    _hover={{
-                                        bg: 'blue.500',
-                                    }}>
-                                    Add Schema
-                                </Button>
-                            </Stack>
-                        </Stack>
-                    </form>
-                </Box>
-            </Stack>
-        </Flex>
+        <>
+            {loading ? (
+                <Loading />
+            ) : (
+                <Flex
+                    minH={'100vh'}
+                    align={'center'}
+                    justify={'center'}
+                >
+                    <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                        <Box
+                            rounded={'lg'}
+                            boxShadow={'lg'}
+                            p={8}>
+                            <form onSubmit={handleSubmit}>
+                                <Stack spacing={4}>
+                                    <FormControl>
+                                        <FormLabel>Schema Name</FormLabel>
+                                        <Input
+                                            autoComplete={"off"}
+                                            placeholder='Schema Name'
+                                            value={schemaName}
+                                            onChange={(e) => setSchemaName(e.target.value)}
+                                        />
+                                        <FormLabel>Schema Version</FormLabel>
+                                        <Input
+                                            autoComplete={"off"}
+                                            placeholder='Schema Version'
+                                            value={schemaVersion}
+                                            onChange={(e) => setSchemaVersion(e.target.value)}
+                                        />
+                                        <FormLabel>Schema Attributes</FormLabel>
+                                        <Input
+                                            autoComplete={"off"}
+                                            placeholder='Attribute A, Attribute B'
+                                            value={schemaAttributes}
+                                            onChange={(e) => setSchemaAttributes(e.target.value)}
+                                        />
+                                    </FormControl>
+                                    <Stack spacing={10}>
+                                        <Button
+                                            type={'submit'}
+                                            bg={'blue.400'}
+                                            color={'white'}
+                                            _hover={{
+                                                bg: 'blue.500',
+                                            }}>
+                                            Add Schema
+                                        </Button>
+                                    </Stack>
+                                </Stack>
+                            </form>
+                        </Box>
+                    </Stack>
+                </Flex>
+            )}
+        </>
     );
 }
 
