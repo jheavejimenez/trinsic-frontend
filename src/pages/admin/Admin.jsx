abc6423e03657ac2aca2febba26f1d0767e159dd
jheave <jimenezjheave123@gmail.com>
2022-09-28 18:05:33 +0800

    feat: fecth certs

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,8 +1,38 @@
 import { Button, Flex, Table, TableContainer, Tbody, Td, Th, Thead, Tr, useColorModeValue, } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
+import { getSumittedApplications } from "../../repository/admin";
 
 function Admin() {
     const [certs, setCerts] = useState([]);
+
+    useEffect(() => {
+        async function fetchCerts() {
+            const res = await getSumittedApplications()
+            setCerts(res)
+        }
+
+        fetchCerts()
+        let interval = setInterval(async () => {
+            fetchCerts()
+        }, 10000);
+        return () => {
+            clearInterval(interval); // need to clear the interval when the component unmounts to prevent memory leaks
+        };
+    }, []);
+
+    const handleApprove = async (cert) => {
+        const isApprove = true;
+
+        await approveApplication(
+            cert._id,
+            cert.firstName,
+            cert.lastName,
+            cert.email,
+            cert.course,
+            isApprove,
+        )
+    }
+
 
     return (
         <Flex
